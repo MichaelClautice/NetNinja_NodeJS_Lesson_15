315b19229a6c7940d1c4cd3865f9b798bc8b1c4a
Michael Clautice <michaelclauticeusa@gmail.com>
2021-06-05 16:45:06 -0400

    Mike modified 2 files & created 2 files Sat.6.5.21.

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # NetNinja_NodeJS_Lesson_15
 Michael Clautice, Richmond, VA<br>
 The Net Ninja - NodeJS Tutorial for Beginners<br>
-Lesson 11 - Clients & Servers
+Lesson 15 - Writable Streams
 
-https://www.youtube.com/watch?v=qSAze9b0wrY&list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4byYp&index=11
+https://www.youtube.com/watch?v=DvlCT0N7yQI&list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4byYp&index=15
 
-• How clients & servers work together to display content to a user.
+• The file system module is required to stream chunks of a file’s data into a buffer then write the data chunks into a new file.
 
-• HTTP, FTP, & TCP Communication protocols
-
-• IP & Port Numbers 
+• Streams of data chunks stored in temporary buffers allows data to be written to a new file before it's fully downloaded into memory which accelerates application performance.
--- app.js
+++ app.js
@@ -2,3 +2,20 @@
 // The Net Ninja - NodeJS Tutorial for Beginners 
 // Lesson 15 - Writable Streams
 // -------
+// to work w my file system I require th file system mod
+const fs = require('fs');
+
+// declare a var tht will stream & decode content FROM readMe.txt
+const myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+
+// declare a var tht will create a destination file & write TO tht destination file 
+const myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+// use th on() event listener on the myReadStream variable to...
+// handle th data event & also execute a call back function 
+// call back fnctn will take a chunk of data &...
+// con.log a test mssg, also...
+// execute th write() mthd on the myWriteStream variable
+myReadStream.on('data', function(chunk){
+    console.log('\nnew chunk received\n');
+ myWriteStream.write(chunk);
+});
